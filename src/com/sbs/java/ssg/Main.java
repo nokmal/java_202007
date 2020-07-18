98d4e7d18eef9399f83fe1cad382df82d2eb2d2e
nokmal <9aedarae@gmail.com>
2020-07-18 21:15:19 +0900

    게시물 존재 유무 탐색

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -59,7 +59,26 @@ public class Main {
 			else if (command.startsWith("article detail")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
-				System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);				
+				
+				boolean found = false; 
+				
+				for (int i = 0 ; i < articles.size(); i++) {
+					Article article = articles.get(i);
+					
+					if ( article.id == id ) {
+						found = true;
+						break;
+					}
+				}
+				
+				if (found == false) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+					continue;
+				}
+				else {
+					System.out.printf("%d번 게시물은 존재합니다.\n", id);
+				}
+								
 			}
 			else {
 				System.out.printf("%s은(는) 존재하지 않는 명령어입니다.\n", command);
