8e5d32723be2686d35ed103ff122377df60c688a
nokmal <9aedarae@gmail.com>
2020-07-18 20:44:36 +0900

    입력된 게시물 저장 및 리스팅 구현

--- src/com/sbs/java/ssg/Main.java
+++ src/com/sbs/java/ssg/Main.java
@@ -1,5 +1,7 @@
 package com.sbs.java.ssg;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -9,6 +11,8 @@ public class Main {
 		System.out.println("== 프로그램 시작 ==");
 		String command;
 		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
 		Scanner sc = new Scanner(System.in);
 		
 		while (true) {
@@ -35,10 +39,23 @@ public class Main {
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
+				Article article = new Article(id, title, body);
+				articles.add(article);
+				
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			}
 			else if (command.equals("article list")) {
-				System.out.println("게시물이 없습니다.");
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				System.out.println("번호 | 제목");
+				for (int i = articles.size() - 1 ; i >= 0 ; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d  | %s\n" , article.id, article.title);
+				}
+				
 			}
 			else {
 				System.out.printf("%s은(는) 존재하지 않는 명령어입니다.\n", command);
@@ -49,4 +66,15 @@ public class Main {
 		System.out.println("== 프로그램 종료 ==");
 		
 	}
+}
+
+class Article {
+	public Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	int id;
+	String title;
+	String body;
 }
