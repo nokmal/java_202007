c9cbeaa01626b26bf5eed2ae6889c6d89822b8f3
nokmal <9aedarae@gmail.com>
2020-07-19 20:07:35 +0900

    게시물 작성 후 조회 시 날짜 출력

--- src/com/sbs/java/ssg/App.java
+++ src/com/sbs/java/ssg/App.java
@@ -17,7 +17,7 @@ public class App {
 			System.out.printf("명령어를 입력해주세요:");
 			command = sc.nextLine();
 			
-			command = command.trim();
+			command = command.trim(); 
 			
 			if (command.length() == 0) {
 				continue;
@@ -31,13 +31,15 @@ public class App {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
 				
+				String regData = Util.getNowDateStr();
+				
 				System.out.println("게시글을 작성합니다.");
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 				
-				Article article = new Article(id, title, body);
+				Article article = new Article(id, regData, title, body);
 				articles.add(article);
 				
 				System.out.printf("%d번 글이 생성되었습니다.\n", id);
@@ -75,7 +77,7 @@ public class App {
 				}
 				
 					System.out.printf("번호 : %d\n", foundArticle.id);
-					System.out.printf("날짜 : 20200718 11:11:11\n");
+					System.out.printf("날짜 : %s\n", foundArticle.regData);
 					System.out.printf("제목 : %s\n", foundArticle.title);
 					System.out.printf("내용 : %s\n", foundArticle.body);				
 			}
@@ -83,19 +85,22 @@ public class App {
 				System.out.printf("%s은(는) 존재하지 않는 명령어입니다.\n", command);
 			}
 		}
-		
 		sc.close();
 		System.out.println("== 프로그램 종료 ==");
 		
 	}
 }
+
 class Article {
-	public Article(int id, String title, String body) {
-		this.id = id;
-		this.title = title;
-		this.body = body;
-	}
 	int id;
 	String title;
 	String body;
+	String regData;
+	
+	public Article(int id, String regData, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+		this.regData = regData;
+	}
 }
---
+++ src/com/sbs/java/ssg/Util.java
@@ -0,0 +1,13 @@
+package com.sbs.java.ssg;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+// 현재 시간 테스트
+public class Util {
+	public static String getNowDateStr() {
+		SimpleDateFormat format = new SimpleDateFormat ( "yyyy-MM-dd HH:mm:ss");
+		Date time = new Date();
+		return format.format(time);
+	}
+}
